ccb650cb34fb72d7e3ad14adb0aa280fd4878bc3
Asad Ali <message.asadali@gmail.com>
2026-01-15 19:59:58 +0500

    added route and controlle to seed todos in db

--- app/controller/seed.controller.js
+++ app/controller/seed.controller.js
@@ -1,4 +1,5 @@
 import PostModel from '../model/post.model.js';
+import TodoModel from '../model/todo.model.js';
 
 export const seedPosts = async (req, res) => {
 	try {
@@ -9,8 +10,26 @@ export const seedPosts = async (req, res) => {
 		const newData = data.map(item => ({
 			title: item.title,
 			body: item.body,
-		})); 
+		}));
 		await PostModel.insertMany(newData);
+
+		res.status(201).send('seeded successfully');
+	} catch (error) {
+		console.error('SEED ERROR:', error);
+		res.status(500).send(error);
+	}
+};
+export const seedTodos = async (req, res) => {
+	try {
+		const url = 'https://jsonplaceholder.typicode.com/todos';
+		const resp = await fetch(url);
+		const data = await resp.json();
+
+		const newData = data.map(item => ({
+			title: item.title,
+			body: item.body,
+		}));
+		await TodoModel.insertMany(newData);
 
 		res.status(201).send('seeded successfully');
 	} catch (error) {
