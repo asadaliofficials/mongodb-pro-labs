84cab96f4b9fdea5360f5040e671b7201641b02b
Asad Ali <asadjattofficials@gmail.com>
2026-01-15 19:54:23 +0500

    added route and controlle to seed db

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { seedPosts } from './controller/seed.controller.js';
 
 const app = express();
 
 app.get('/', (req, res) =>{
   res.send('Hello World');
 })
+
+app.get('/seed/posts', seedPosts)
 
 export default app;
