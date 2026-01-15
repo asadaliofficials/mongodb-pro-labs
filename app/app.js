400df534c37c9da9d3f77fa3724a11cee5724399
Asad Ali <asadjattofficials@gmail.com>
2026-01-15 23:12:03 +0500

    fetch only products that have price greater than 50

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { seedPosts, seedProducts, seedTodos } from './controller/seed.controller.js';
+import { getProducts } from './controller/products.controller.js';
 
 const app = express();
 
@@ -7,8 +8,12 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+// routes to seed the database with dummy data
 app.get('/seed/posts', seedPosts);
 app.get('/seed/todos', seedTodos);
 app.get('/seed/products', seedProducts);
+
+// routes to get products
+app.get('/products', getProducts)
 
 export default app;
